233008e7d98bca768c8e6ee3fd96f488fd183916
mark072205 <sendiong568@gmail.com>
2026-04-21 14:30:19 +0800

    refractor: Migration JS to TS

--- App.tsx
+++ App.tsx
@@ -7,6 +7,7 @@ import rootSaga from './src/app/sagas';
 import configureStore from './src/app/reducers';
 import { Provider } from 'react-redux';
 
+//JS to TSX conversion, with added comments for clarity and maintainability. The App component sets up the Redux store and provides it to the rest of the app, while also rendering the main navigation component.
 // NOTE: redux-persist disabled in `src/app/reducers/index.ts`.
 // const { store, persistor, runSaga } = configureStore();
 const { store, runSaga } = configureStore();
